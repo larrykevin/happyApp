f7df5cb2cd379783ec2173f3609593f56b9ec849
Larry Kevin <larry.kevin.c.c@gmail.com>
2020-09-06 22:15:45 -0500

    correcting the first bug

--- js/index.js
+++ js/index.js
@@ -3,27 +3,47 @@ let button =  document.getElementById('buttonSubmit');
 
 function saludar() {
    let nombreUsuario =  userValue.value;
-   let malAugurio = 'Ingresa tu nombre bien no mames ya te levantas y la estas regando :V';
+   let malAugurio = 'Ingresa bien tu nombre! no mames ya te levantas y la estas regando :V';
    let imprimirSuerte = document.getElementById('imprimirSuerte');
 
-   if(nombreUsuario.length > 1 && nombreUsuario.length < 4 ) {
+   if(nombreUsuario.length >= 1 && nombreUsuario.length < 3 ) {
       imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase1}`;
    } 
-   else if (nombreUsuario.length >= 4 && nombreUsuario.length < 9 ) {
+   else if (nombreUsuario.length === 3 ) {
       imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase2}`;
    } 
-   else if (nombreUsuario.length >= 9 ) {
+   else if (nombreUsuario.length === 4 ) {
       imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase3}`;
-   } 
+   }
+   else if (nombreUsuario.length === 5 ) {
+      imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase4}`;
+   }
+   else if (nombreUsuario.length === 6 ) {
+      imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase5}`;
+   }
+   else if (nombreUsuario.length === 7 ) {
+      imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase6}`;
+   }
+   else if (nombreUsuario.length === 8 ) {
+      imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase7}`;
+   }
+   else if (nombreUsuario.length >= 9 ) {
+      imprimirSuerte.innerHTML = `Hola ${nombreUsuario}, ${this.frase8}`;
+   }
    else {
       imprimirSuerte.innerHTML = `${malAugurio}`;
    }
 }
 
 const frases = {
-   frase1: `te va a ir muy cabron eso no lo dudes`,
-   frase2: `así se te cruce un gato negro nadie te detiene`,
-   frase3: `casi casi te ponen de nombre esternocleidomastoideo xD. Pero igual eres el mejor!!`
+   frase1: `tu nombre es muy corto, pero tu día y tu felicidad serán muy grandes. 	&#128516;`,
+   frase2: `comienza ahora a ser desde hoy lo que serás en el futuro. 	&#128516;`,
+   frase3: `así se te cruce un gato negro, nada te detiene 	&#128516;`,
+   frase4: `recuerda siempre, progreso antes de perfección. Vamos tu puedes! 	&#128516;`,
+   frase5: `la magia es creer en ti mismo. Si puedes hacer eso, puedes hacer que suceda cualquier cosa. 	&#128516;`,
+   frase6: `todo lo que siempre has querido está al otro lado del miedo. 	&#128516;`,
+   frase7: `así se te cruce un gato negro nadie te detiene 	&#128516;`,
+   frase8: `casi casi te ponen de nombre esternocleidomastoideo xD. Pero igual eres el mejor!! 	&#128516;`
 }
 
 button.addEventListener('click', function() {
